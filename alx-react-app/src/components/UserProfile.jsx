64f6359edcb75ae4aaf393da790a2f48bca290d9
waweru <alvinwaweru43@gmail.com>
2026-01-18 23:17:22 +0300

    refactor: convert UserProfile to arrow function and simplify props usage

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
-export default function UserProfile(props) {
-	const { name, age, bio } = props;
-	return (
-		<div className="user-profile">
-			<h2>{name}</h2>
-			<p>Age: {age}</p>
-			<p>{bio}</p>
-		</div>
-	);
-}
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
 
+export default UserProfile;
