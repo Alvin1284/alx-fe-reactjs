421f8be55d9693f19753f99b520eca234dea1264
waweru <alvinwaweru43@gmail.com>
2026-01-18 23:15:43 +0300

    feat: add UserProfile component to display user information

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+export default function UserProfile(props) {
+	const { name, age, bio } = props;
+	return (
+		<div className="user-profile">
+			<h2>{name}</h2>
+			<p>Age: {age}</p>
+			<p>{bio}</p>
+		</div>
+	);
+}
+
